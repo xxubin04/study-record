85e55e655146e5adeba99874ead99c0a5df53a76
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-03-23 17:37:05 +0900

    Update prob07.py

--- algorithms/major-assignment/assignment01/prob07.py
+++ algorithms/major-assignment/assignment01/prob07.py
@@ -5,18 +5,20 @@
 def recursion(prior, idx):
     global cnt
 
-    if idx == N:
-        return
+    if idx == N:  # 인덱스가 N이라면
+        return  # 함수 탈출
 
     if prior == 0:  # 앞의 수가 0이라면, 다음에 1만 올 수 있음
-        recursion(1, idx+1)
+        recursion(1, idx+1)  # 1만 올 수 있다면 경우의 수는 그대로 유지
     else:  # 앞의 수가 1이라면, 다음에 0과 1이 올 수 있으므로 cnt + 1
         cnt += 1
         recursion(0, idx+1)
         recursion(1, idx+1)
 
-cnt = 1  # 이진수열의 개수
+cnt = 1  # 이진수열의 개수 (N이 양의 정수라고 했으므로 최소 1이다)
 N = int(input())
 
-recursion(1, 0)  # N이 1일때는 0과 1이 가능하므로 prior(앞의 수)를 1로 지정
+# N이 1일때는 0과 1이 가능하므로 prior(앞의 수)를 1로 지정
+# N >= 1 이라면, 이진수열의 가장 큰 자릿수는 1일 수 밖에 없음
+recursion(1, 0)
 print(cnt)
