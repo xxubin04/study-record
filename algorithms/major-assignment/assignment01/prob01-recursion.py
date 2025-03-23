5fafc587b930e88b1b000ecde5f0d963e951d7c5
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-03-23 17:33:16 +0900

    Update prob01-recursion.py

--- algorithms/major-assignment/assignment01/prob01-recursion.py
+++ algorithms/major-assignment/assignment01/prob01-recursion.py
@@ -1,15 +1,13 @@
-# 수정 필요
-def recursion(nums, i, K, cnt):
-    if i == len(nums) - 1:
-        return cnt
-    elif nums[i] < K:
-        print(nums[i], cnt)
-        return recursion(nums, i+1, K, cnt+1)
-    else:
-        print(nums[i], cnt)
-        return recursion(nums, i+1, K, cnt)
+def recursion(arr, i, k, cnt):
+    if i == len(arr):  # 배열의 모든 정수를 탐색했다면
+        return cnt  # cnt 반환
+    elif arr[i] < k:  # 정수가 K보다 작다면
+        return recursion(arr, i+1, k, cnt+1)  # cnt 1 증가
+    else:  # 정수가 K보다 크거나 같다면
+        return recursion(arr, i+1, k, cnt)  # cnt 유지
 
-N = int(input())
-nums = list(map(int, input().split()))
+N = int(input())  # 정수 개수 N 입력
+nums = list(map(int, input().split()))  # 정수들을 배열에 입력
 
+# 인덱스 0번부터, cnt를 1로 설정하여 재귀적으로 탐색 시작
 print(recursion(nums, 0, (K := int(input())), 1))
