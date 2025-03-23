f7f41a30c26f478590d3d4ca12f6e46ab3d3874e
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-03-23 17:38:10 +0900

    Update prob09.py

--- algorithms/major-assignment/assignment01/prob09.py
+++ algorithms/major-assignment/assignment01/prob09.py
@@ -3,30 +3,31 @@
 # 3. 팀의 최대 능력치 출력
 
 
-from itertools import combinations
-
-filename = 'input9.txt'  # 파일 읽기
+filename = 'input10.txt'
 f = open(filename, 'r')
 lines = f.readlines()
-
-ability_list = []
+info = [0]  # info = [(부피, 가격, 폐기비용)] 저장
 
 for i in range(len(lines)):
     if i == 0:
-        N = int(lines[i].rstrip())  # N: 선수 후보 인원수
+        N = int(lines[i].rstrip())  # N: 물건의 개수
     elif i == N+1:
-        k = int(lines[i].rstrip())  # k: 출전 팀 인원수
-    else:
-        ability_list.append(list(map(int, lines[i].rstrip().split())))  # ability: N x N 능력치 배열
+        W = int(lines[i].rstrip())  # W: 최대 부피의 총량
+    else:  # 물건당 (부피, 가격, 폐기비용) 입력받음
+        info.append(list(map(int, lines[i].rstrip().split())))
 
-comb = list(combinations(list(range(0, N)), k))
-max_ability = -999999999
+dp = [[0] * (W+1) for _ in range(N+1)]
 
-for c in comb:  # 가능한 모둔 조합 탐색
-    ability_sum = 0  # 능력치는 0으로 초기화
-    for a in c:  # 조합에 속한 선수들의 능력치 계산
-        for b in c:
-            ability_sum += ability_list[a][b]
-    max_ability = max(max_ability, ability_sum)  # 최대 능력치로 갱신
+for i in range(1, N+1):  # 물건들 순차적으로 확인
+    for j in range(W+1):  # 부피별로 확인
+        # 물건을 버리는 경우의 [가져가는 물건의 가격 합 - 가져가지 않는 물건의 폐기 비용]의 최댓값
+        # i번째 물건을 버리는 경우의 값 = i-1번째 물건까지 확인했을 때의 최댓값 - i번째 물건을 버리는 폐기비용
+        case_dump = dp[i-1][j] - info[i][2]
+        # 현재 물건을 가져가도 부피의 총량 W를 초과하지 않는 경우(남아있는 부피 >= 추가할 물건의 부피)
+        if j >= info[i][0]:
+            case_bring = dp[i-1][j - info[i][0]] + info[i][1]
+            dp[i][j] = max(case_dump, case_bring)
+        else:  # 현재 물건의 부피보다 남아있는 부피의 총량이 작은 경우
+            dp[i][j] = case_dump  # 물건을 가져가지 않음
 
-print(max_ability)  # 팀의 최대 능력치 출력
+print(dp[N][W])  # 모든 물건들을 순회하고 난 최종 결과 출력
